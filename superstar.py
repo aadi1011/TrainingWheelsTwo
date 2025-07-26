470b6431e87ac3a278981ea6a3e1bce9e506db4e
ATS-27 <atherva.srivastava.btech2024@sitpune.edu.in>
2025-07-26 12:28:35 +0530

    Atherva's Commit

--- superstar.py
+++ superstar.py
@@ -6,3 +6,32 @@ def greet(name):
 def add(a, b):
     """Function to add two numbers."""
     return a + b
+
+def subtract(a, b):
+    """Function to subtract two numbers."""
+    return a - b
+
+def multiply(a, b):
+    """Function to multiply two numbers."""
+    return a * b
+
+def divide(a, b):
+    """Function to divide two numbers."""
+    return a / b
+
+def power(a, b):
+    """Function to raise a number to the power of another number."""
+    return a ** b
+
+def modulus(a, b):
+    """Function to return the remainder of a number divided by another number."""
+    return a % b
+
+
+def floor_division(a, b):
+    """Function to perform floor division of two numbers."""
+    return a // b
+
+def square_root(a):
+    """Function to return the square root of a number."""
+    return a ** 0.5
